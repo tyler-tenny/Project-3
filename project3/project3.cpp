b4b38ad61a85bc1d58c79679ffa08ef737c6ac01
Tyler Tenny <tyler.tenny@oit.edu>
2022-02-16 16:38:17 -0800

    fetch test

--- project3/project3.cpp
+++ project3/project3.cpp
@@ -7,6 +7,7 @@ int main()
 {
     std::cout << "Hello World!\n";
     std::cout << "Commit test";
+    std::cout << "\nFetch Test";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
